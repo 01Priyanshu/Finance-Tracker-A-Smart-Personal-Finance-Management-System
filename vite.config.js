c3e65296bc9bf6e3e1f68bb036d5e00756c4ccf1
Priyanshu Patra <priyanshupatra22072002@gmail.com>
2025-03-11 15:25:29 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import removeConsole from "vite-plugin-remove-console";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "production" && removeConsole()].filter(Boolean),
-  server: {
-    host: true,
-    strictPort: true,
-    port: 8080,
-    proxy: {
-      "/api/v1": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src', // Ensure this alias points to your src directory
     },
   },
-}));
+  build: {
+    rollupOptions: {
+      input: '/index.html', // Ensure Vite correctly identifies entry point
+    },
+  },
+});
