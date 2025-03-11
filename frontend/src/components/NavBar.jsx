8ec852c62e00cb21299fb3d7e7be5cc84876fd3a
Priyanshu Patra <priyanshupatra22072002@gmail.com>
2025-03-11 21:13:19 +0530

    Update NavBar.jsx

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -21,16 +21,16 @@ const NavBar = () => {
           className="text-xl sm:text-2xl md:text-3xl uppercase cursor-pointer"
           onClick={() => navigate("/")}
         >
-          Spend{" "}
+          Finance{" "}
           <span className="text-primary text-base sm:text-xl md:text-2xl">
-            Smart.
+            Tracker
           </span>
         </h1>
       </div>
       <div className="hidden xmd:flex items-center space-x-4">
         <img src={arrow} alt="" className="w-[35px]" />
         <Link
-          to="https://www.linkedin.com/in/saxena-shourya/"
+          to="https://www.linkedin.com/in/priyanshu-patra-01gg/"
           target="_blank"
           className="text-xl text-secondary transition-all hover:text-primary relative animateBottom"
         >
